1489716c9b8e8d2232810aa3295f511b8767d5ae
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-15 22:56:22 -0400

    route code cleanup

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,39 @@
+// >description: get book links
+// route is Get / api/user-routes
+// Access is Private: (for now)
+
+const getUsers = (req, res) => {
+    res.status(200).json({ nessage: 'getting links '})
+}
+
+// >description: set book links
+// route is Post / api/user-routes
+// Access is Private: (for now)
+
+const setUser = (req, res) => {
+    res.status(200).json({ nessage: 'adding links '})
+}
+
+// >description: updating book links
+// route is Put / api/user-routes/:id
+// Access is Private: (for now)
+
+const putUsers = (req, res) => {
+    res.status(200).json({ nessage: `Update links ${req.params.id}`})
+}
+
+// >description: deleting book links
+// route is Delete / api/user-routes/:id
+// Access is Private: (for now)
+
+const deleteUsers = (req, res) => {
+    res.status(200).json({ nessage: `Delete links ${req.params.id}`})
+}
+
+
+module.exports ={
+    getUsers,
+    setUser,
+    putUsers,
+    deleteUsers,
+}
--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const { getUsers, 
+    setUser, 
+    putUsers, 
+    deleteUsers
+ } = require('../../controllers/user-controller')
 
-router.get('/', (req, res) =>{
-    res.status(200).json({ nessage: 'getting links '})
-})
-router.post('/', (req, res) =>{
-    res.status(200).json({ nessage: 'adding links '})
-})
-router.put('/:id', (req, res) =>{
-    res.status(200).json({ nessage: `Update links ${req.params.id}`})
-})
-router.delete('/:id', (req, res) =>{
-    res.status(200).json({ nessage: `Delete links ${req.params.id}`})
-})
+ 
+router.get('/', getUsers)
+
+router.post('/', setUser)
+
+router.put('/:id',  putUsers)
+
+router.delete('/:id', deleteUsers)
 
 module.exports = router
