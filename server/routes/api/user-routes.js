1e3853e4bdf8aadec836d581fb9aa2e32dec1987
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-15 22:41:58 -0400

    updating user-routes to create CRUD functions

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,4 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) =>{
+    res.status(200).json({ nessage: 'getting links '})
+})
+router.post('/', (req, res) =>{
+    res.status(200).json({ nessage: 'adding links '})
+})
+router.put('/:id', (req, res) =>{
+    res.status(200).json({ nessage: `Update links ${req.params.id}`})
+})
+router.delete('/:id', (req, res) =>{
+    res.status(200).json({ nessage: `Delete links ${req.params.id}`})
+})
+
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ const port = process.env.PORT || 3001
 
 const app = express()
 
-app.get('/api/user-routes', (req, res) =>{
-    res.json({ nessage: 'getting links '})
-})
+app.use('/api/user-routes', require('./routes/api/user-routes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
