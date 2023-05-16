ba93dc1d93610984af46ff9052f39acd8ed2babc
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-15 22:35:24 -0400

    updating user-routes.js

--- server/server.js
+++ server/server.js
@@ -4,4 +4,10 @@ const port = process.env.PORT || 3001
 
 const app = express()
 
+app.get('/api/user-routes', (req, res) =>{
+    res.json({ nessage: 'getting links '})
+})
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
+
+
