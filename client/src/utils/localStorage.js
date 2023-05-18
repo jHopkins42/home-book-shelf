662f1aafe14dd21093d0dce751e781a7c773c019
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-18 00:21:05 -0400

    local storage utility build

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,2 +1,37 @@
-//not built at this time
->
+// >descrition: exporting book queries
+
+export const getSavedBookIds = () => {
+    const savedBookIds = localStorage.getItem('saved_books')
+      ? JSON.parse(localStorage.getItem('saved_books'))
+      : [];
+  
+    return savedBookIds;
+  };
+  
+// >description: exporting saved book by ID
+
+  export const saveBookIds = (bookIdArr) => {
+    if (bookIdArr.length) {
+      localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
+    } else {
+      localStorage.removeItem('saved_books');
+    }
+  };
+  
+// >description: exporting removal of book by ID
+
+  export const removeBookId = (bookId) => {
+    const savedBookIds = localStorage.getItem('saved_books')
+      ? JSON.parse(localStorage.getItem('saved_books'))
+      : null;
+  
+    if (!savedBookIds) {
+      return false;
+    }
+  
+    const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
+    localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
+  
+    return true;
+  };
+  
