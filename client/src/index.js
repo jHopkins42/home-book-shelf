f676fca8a56614fef45ca298a2bb00e34ab569d4
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-18 00:14:55 -0400

    client/src styling and html

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+//imports/required
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+
+//renders
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
