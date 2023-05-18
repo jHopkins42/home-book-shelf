eec9d9a8c5581d1b9a6d18a14746dc887f396e84
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-17 23:17:51 -0400

    enter book schema info

---
+++ server/models/book.js
@@ -0,0 +1,46 @@
+//required imports
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+
+    },
+    author: {
+        type: String,
+        required: true,
+
+    },
+    description: {
+        type: String,
+        required: false,
+
+    },
+    price: {
+        type: Number,
+        required: true,
+
+    },
+    available: {
+        type: Boolean,
+
+    },
+    genre: {
+        type: String,
+        required: true,
+
+    }, 
+    image: {
+        type: String,
+        required: true,
+
+    },
+});
+
+//exports
+module.exports = mongoose.model("Book", bookSchema);
+// room left to add additional information per schema category.
+//potential additional items to track: quantity owned, condition, format (hardback/paperback/e-book)
