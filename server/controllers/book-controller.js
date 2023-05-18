c572ab0cd088fe4d1759aa7f90498c8844fda971
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-17 23:27:19 -0400

    initial book-controller code

---
+++ server/controllers/book-controller.js
@@ -0,0 +1,122 @@
+//required imports/constants
+const asyncHandler = require('express-async-handler');
+const Book = require("../models/book");
+
+// >description: get all the books
+//route is get / api/book-routes
+//access is private for now
+const getAllBooks = async (req, res, next) => {
+  let books;
+  try {
+    books = await Book.find();
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (!books) {
+    return res.status(404).json({ message: "No products found" });
+  }
+  return res.status(200).json({ books });
+};
+
+
+// >description: get specific books
+//route is get / api/book-routes
+//access is private for now
+const getById = async (req, res, next) => {
+  const id = req.params.id;
+  let book;
+  try {
+    book = await Book.findById(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "No Book found" });
+  }
+  return res.status(200).json({ book });
+};
+
+
+// >description: add a books
+//route is get / api/book-routes
+//access is private for now
+const addBook = async (req, res, next) => {
+  const { name, author, description, price, available, image } = req.body;
+  let book;
+  try {
+    book = new Book({
+      name,
+      author,
+      description,
+      price,
+      available,
+      image,
+    });
+    await book.save();
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (!book) {
+    return res.status(500).json({ message: "Unable To Add" });
+  }
+  return res.status(201).json({ book });
+};
+
+
+// >description: update a books
+//route is get / api/book-routes
+//access is private for now
+const updateBook = async (req, res, next) => {
+  const id = req.params.id;
+  const { name, author, description, price, available, image } = req.body;
+  let book;
+  try {
+    book = await Book.findByIdAndUpdate(id, {
+      name,
+      author,
+      description,
+      price,
+      available,
+      image,
+    });
+    book = await book.save();
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "Unable To Update By this ID" });
+  }
+  return res.status(200).json({ book });
+};
+
+
+// >description: delete specific books
+//route is get / api/book-routes
+//access is private for now
+// but why would you ever delete a book?
+
+const deleteBook = async (req, res, next) => {
+  const id = req.params.id;
+  let book;
+  try {
+    book = await Book.findByIdAndRemove(id);
+  } catch (err) {
+    console.log(err);
+  }
+  if (!book) {
+    return res.status(404).json({ message: "Unable To Delete By this ID" });
+  }
+  return res.status(200).json({ message: "Product Successfully Deleted" });
+};
+
+//exports
+
+module.exports = {
+    getAllBooks,
+    SetBooks,
+    putBooks,
+    deleteBooks,
+    getBookByID,
+}
