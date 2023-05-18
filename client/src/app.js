c62a4241a5220469a9be3311f6760e0a4af9003a
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-18 00:16:27 -0400

    client/src app.js build

---
+++ client/src/app.js
@@ -0,0 +1,26 @@
+//imports/required
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+
+//gunctions
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={SearchBooks} />
+          <Route exact path='/saved' component={SavedBooks} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
+  );
+}
+
+//exports
+
+export default App;
