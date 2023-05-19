d6b5f69615999b91dd3a3759cd0d2e1b64b62fd0
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-18 21:27:55 -0400

    connection syntax bugfix and spelling

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,4 +5,4 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_CNCT || 'mongodb://127.0.0.1:27017/googlebooks');
 
 //exports and tea and crumpets
-module.export - MongoDBNamespace.connection;
+module.exports = mongoose.connection;
