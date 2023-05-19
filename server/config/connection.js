fa154c432dbbf3a11ec6d2ba6123c3928418d323
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-18 21:25:39 -0400

    server.config.connection file established

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,8 @@
->
+//imports / constants / required
+const mongoose = require('mongoose');
+
+//dotenv connection
+mongoose.connect(process.env.MONGODB_CNCT || 'mongodb://127.0.0.1:27017/googlebooks');
+
+//exports and tea and crumpets
+module.export - MongoDBNamespace.connection;
